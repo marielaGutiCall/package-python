b7e40cc47bb417e7acb0676d393b424ea793bf1a
Mariela Gutierrez Callejas <mariela.guticall@gmail.com>
2022-06-02 19:53:10 -0400

    Workshops Module

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
+from mgc_python_package import workshops
+from mgc_python_package.workshops import unreleased
 if __name__ == '__main__':
    print('hello')
+   workshops = unreleased();
+   print(workshops)
