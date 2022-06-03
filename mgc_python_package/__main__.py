4e35a05cd569cc1ce2cae953873937cef2062eb9
Mariela Gutierrez Callejas <mariela.guticall@gmail.com>
2022-06-02 20:23:40 -0400

    logging module

--- mgc_python_package/__main__.py
+++ mgc_python_package/__main__.py
@@ -1,7 +1,17 @@
+import logging
 from mgc_python_package import unreleased
 
+'''
+ menssages:
+ info  --> 10
+ debug --> 20
+ warning --> 30
+ error --> 40
+ critical --> 50
+'''
+logging.basicConfig(level=logging.DEBUG)
 if __name__ == '__main__':
-   print('>>> Starting the package....')
+   logging.debug('>>> Starting the package....')
    workshops = unreleased();
-   print(workshops)
-   print('>>> Finishing the package....')
+   logging.debug(workshops)
+   logging.debug('>>> Finishing the package....')
