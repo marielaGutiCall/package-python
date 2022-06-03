685ffdaa50dd4f44fbc1dd65beb0b38dcf5e19f2
Mariela Gutierrez Callejas <mariela.guticall@gmail.com>
2022-06-02 22:03:52 -0400

    adding license and setup files

--- mgc_python_package/__main__.py
+++ mgc_python_package/__main__.py
@@ -10,8 +10,11 @@ from mgc_python_package import unreleased
  critical --> 50
 '''
 logging.basicConfig(level=logging.DEBUG)
+# execute everything in the block if and only if this file is executed as main
 if __name__ == '__main__':
    logging.debug('>>> Starting the package....')
    workshops = unreleased();
+   #logging.debug(unreleased.__doc__)
+   logging.debug(help(unreleased))
    logging.debug(workshops)
    logging.debug('>>> Finishing the package....')
